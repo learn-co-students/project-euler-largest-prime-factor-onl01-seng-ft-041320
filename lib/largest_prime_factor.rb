25b94e2c9bc9950060cfe84fbf36048d9e72962e
rschlosser11 <rosalinschlosser@gmail.com>
2020-10-05 20:05:06 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(num) 
+    factors = get_factors(num);
+    factors.sort.last
+end
+
+def get_factors(num) 
+    i = 2
+    factors = [];
+    while i < Math.sqrt(num)
+        if num % i == 0 && Prime.prime?(i)
+            factors << i
+        end
+        i += 1
+    end
+    factors
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,21 @@
-# Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+    attr_accessor :num
+
+    def initialize(num)
+        @num = num;
+    end
+
+    def number
+        i = 2
+        factors = [];
+        while i < Math.sqrt(@num)
+            if @num % i == 0 && Prime.prime?(i)
+                factors << i
+            end
+            i += 1
+        end
+        factors.sort.last;
+    end
+end
